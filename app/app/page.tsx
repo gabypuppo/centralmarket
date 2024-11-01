06fb9128ee4fb654c3c3a71bf579421e12925e1d
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-01 14:57:21 -0300

    App fix error (when user has no orders)

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -6,7 +6,12 @@ import type { Order } from '@/db/orders'
 import { getLatestOrderBuyer, getLatestOrderUser } from '@/db/orders'
 import { auth } from '@/auth'
 import NotFoundError from '@/components/error/NotFountError'
-import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from '@/components/ui/Table'
+import { TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table, } from '@/components/ui/Table'
 import { getOrdersByBuyer, getOrdersByUser } from '@/db/orders'
 
 export default async function Page() {
@@ -18,15 +23,18 @@ export default async function Page() {
   if (!id)
     return (
       <NotFoundError>
-        <p className="text-center text-muted-foreground">Error al traer los datos del usuario.</p>
+        <p className="text-center text-muted-foreground">
+          Error al traer los datos del usuario.
+        </p>
       </NotFoundError>
     )
-
 
   if (!organizationId)
     return (
       <NotFoundError>
-        <p className="text-center text-muted-foreground">Este usuario no esta asignado a ninguna organización.</p>
+        <p className="text-center text-muted-foreground">
+          Este usuario no esta asignado a ninguna organización.
+        </p>
       </NotFoundError>
     )
 
@@ -43,14 +51,13 @@ export default async function Page() {
   } else {
     allOrders = await getOrdersByUser(id!)
   }
-  
+
   const sortOrders = <T extends Order>(orders: T[]) =>
     orders.sort(
       (a, b) =>
-        (a.shippingDate?.getTime() ?? Date.now()) - (b.shippingDate?.getTime() ?? Date.now())
+        (a.shippingDate?.getTime() ?? Date.now()) -
+        (b.shippingDate?.getTime() ?? Date.now()),
     )
-
-
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -66,7 +73,9 @@ export default async function Page() {
           )}
           <div className="grid gap-4">
             <div className="flex items-center justify-between">
-              <h2 className="text-2xl font-bold">Pedidos que esperan tu respuesta</h2>
+              <h2 className="text-2xl font-bold">
+                Pedidos que esperan tu respuesta
+              </h2>
               <Link
                 href="/app/orders"
                 className="text-sm font-medium text-primary hover:underline"
@@ -80,22 +89,27 @@ export default async function Page() {
                 <p className=" text-muted-foreground">
                   No hay pedidos que esperen tu respuesta.
                 </p>
-              </div>    
+              </div>
             )}
             {orders?.map((order, index) => (
-              <div key={index} className="flex flex-row bg-background text-sm p-2 relative">
+              <div
+                key={index}
+                className="flex flex-row bg-background text-sm p-2 relative"
+              >
                 <div className="p-2 flex flex-col gap-1 flex-1 min-w-20">
                   <h4 className="font-semibold">#{order.id}</h4>
                   <p className="truncate">{order.notes}</p>
                 </div>
                 <div className="p-2 flex gap-1 flex-1 min-w-20 items-center">
                   {order?.orderStatus && (
-                    <Badge className={getFormattedColors(order?.orderStatus)}>{getFormattedLabel(order?.orderStatus)}</Badge>
+                    <Badge className={getFormattedColors(order?.orderStatus)}>
+                      {getFormattedLabel(order?.orderStatus)}
+                    </Badge>
                   )}
                 </div>
                 <div className="p-2 flex gap-1 flex-1 min-w-20 items-center">
                   <p className="">
-                    {order.shippingDate?.toLocaleDateString('es-US', { dateStyle: 'long' })}
+                    {order.shippingDate?.toLocaleDateString('es-US', { dateStyle: 'long', })}
                   </p>
                 </div>
                 <div className="p-2 flex gap-1">
@@ -108,60 +122,71 @@ export default async function Page() {
           </div>
         </div>
         <div className="grid gap-6 max-w-6xl w-full mx-auto">
-          <div className="overflow-x-auto grid gap-4 lg:gap-px lg:bg-gray-50">
+          <div className="overflow-x-auto grid gap-4 mt-4 lg:gap-px lg:bg-gray-50">
             <span>
               <h2 className="text-2xl mt-8 font-bold">Tus pedidos</h2>
             </span>
-            <Table className="border mt-4 rounded-lg">
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Orden</TableHead>
-                  <TableHead>Estado</TableHead>
-                  <TableHead>Fecha de Entrega</TableHead>
-                  <TableHead>Acciones</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {!sortOrders(allOrders).length && (
-                  <div className="p-4 bg-background border border-border rounded-lg">
-                    <p className=" text-muted-foreground">
-                      No hay pedidos que esperen tu respuesta.
-                    </p>
-                  </div>  
-                )}
-                {sortOrders(allOrders).map((order, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <div className="flex flex-col gap-1 flex-1 min-w-20">
-                        <h4 className="font-semibold">#{order.id}</h4>
-                        <h5 className="truncate max-w-48">{order.title || <span className='text-gray-500 font-semibold'>(Sin titulo)</span>}</h5>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex gap-1 flex-1 min-w-20 items-center">
-                        {order?.orderStatus && (
-                          <Badge className={getFormattedColors(order.orderStatus)}>{getFormattedLabel(order.orderStatus)}</Badge>
-                        )}
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex gap-1 flex-1 min-w-20 items-center">
-                        <p className="">
-                          {order.shippingDate?.toLocaleDateString('es-US', { dateStyle: 'long' })}
-                        </p>
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex gap-1">
-                        <Link href={'/app/orders/' + order.id}>
-                          <Button>Detalle</Button>
-                        </Link>
-                      </div>
-                    </TableCell>
+            {allOrders.length === 0 ? (
+              <div className="p-4 bg-background border border-border rounded-lg">
+                <p className=" text-muted-foreground">
+                  No hay pedidos que esperen tu respuesta.
+                </p>
+              </div>
+            ) : (
+              <Table className="border mt-4 rounded-lg">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Orden</TableHead>
+                    <TableHead>Estado</TableHead>
+                    <TableHead>Fecha de Entrega</TableHead>
+                    <TableHead>Acciones</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>  
+                </TableHeader>
+                <TableBody>
+                  {sortOrders(allOrders).map((order, index) => (
+                    <TableRow key={index}>
+                      <TableCell>
+                        <div className="flex flex-col gap-1 flex-1 min-w-20">
+                          <h4 className="font-semibold">#{order.id}</h4>
+                          <h5 className="truncate max-w-48">
+                            {order.title || (
+                              <span className="text-gray-500 font-semibold">
+                                (Sin titulo)
+                              </span>
+                            )}
+                          </h5>
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex gap-1 flex-1 min-w-20 items-center">
+                          {order?.orderStatus && (
+                            <Badge
+                              className={getFormattedColors(order.orderStatus)}
+                            >
+                              {getFormattedLabel(order.orderStatus)}
+                            </Badge>
+                          )}
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex gap-1 flex-1 min-w-20 items-center">
+                          <p className="">
+                            {order.shippingDate?.toLocaleDateString('es-US', { dateStyle: 'long', })}
+                          </p>
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex gap-1">
+                          <Link href={'/app/orders/' + order.id}>
+                            <Button>Detalle</Button>
+                          </Link>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
           </div>
         </div>
       </main>
