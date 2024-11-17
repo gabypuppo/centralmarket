db561fa8f7c40ab69fa9b12adf0668ce24e77e94
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-16 21:29:56 -0300

    organization analytics page parallel data fetching (better performance)

--- app/app/organization/(tabs)/analytics/page.tsx
+++ app/app/organization/(tabs)/analytics/page.tsx
@@ -12,26 +12,40 @@ export default async function Page() {
 
   const currentDate = new Date()
 
-  const weeklyAnalytics = await getAnalyticsByOrganizationId(
+  const weeklyAnalyticsPromise = getAnalyticsByOrganizationId(
     session.user.organizationId,
     new Date(new Date().setDate(currentDate.getDate() - 7))
   )
-  const monthlyAnalytics = await getAnalyticsByOrganizationId(
+  const monthlyAnalyticsPromise = getAnalyticsByOrganizationId(
     session.user.organizationId,
     new Date(new Date().setMonth(currentDate.getMonth() - 1))
   )
-  const yearlyAnalytics = await getAnalyticsByOrganizationId(
+  const yearlyAnalyticsPromise = getAnalyticsByOrganizationId(
     session.user.organizationId,
     new Date(new Date().setFullYear(currentDate.getFullYear() - 1))
   )
 
-  const organizationAnalyticsMonthly = await getMonthlyAnalyticsByOrganizationId(
+  const organizationAnalyticsMonthlyPromise = getMonthlyAnalyticsByOrganizationId(
     session.user.organizationId,
     new Date(new Date().setMonth(currentDate.getMonth() - 12))
   )
 
-  const usersAnalytics = await getOrganizationUsersOrderAnalytics(session.user.organizationId)
-  console.log(usersAnalytics)
+  const usersAnalyticsPromise = getOrganizationUsersOrderAnalytics(session.user.organizationId)
+
+  const [
+    weeklyAnalytics,
+    monthlyAnalytics,
+    yearlyAnalytics,
+    organizationAnalyticsMonthly,
+    usersAnalytics
+  ] = await Promise.all([
+    weeklyAnalyticsPromise,
+    monthlyAnalyticsPromise,
+    yearlyAnalyticsPromise,
+    organizationAnalyticsMonthlyPromise,
+    usersAnalyticsPromise
+  ])
+
   return (
     <>
       <Card className="flex-1 min-w-96">
@@ -105,7 +119,11 @@ export default async function Page() {
           {usersAnalytics.map((userAnalytics, i) => (
             <TableRow key={i}>
               <TableCell>
-                <p className="font-semibold">{userAnalytics.createdBy ? `${userAnalytics.createdBy?.firstName} ${userAnalytics.createdBy?.lastName}` : 'Usuario Desconocido'}</p>
+                <p className="font-semibold">
+                  {userAnalytics.createdBy
+                    ? `${userAnalytics.createdBy?.firstName} ${userAnalytics.createdBy?.lastName}`
+                    : 'Usuario Desconocido'}
+                </p>
                 <p>{userAnalytics.createdBy?.email}</p>
               </TableCell>
               <TableCell>
