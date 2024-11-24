424afd851eb1e9f8ed2ef7185f37657bff8ae74b
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-23 21:06:26 -0300

    fix TabsNav grid flow auto to col

--- app/components/layout/TabsNav/index.tsx
+++ app/components/layout/TabsNav/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function TabsNav({ tabs }: Props) {
   return (
     <nav
-      className={`h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground grid w-full grid-cols-${tabs.length}`}
+      className={`h-10 items-center rounded-md bg-muted p-1 text-muted-foreground w-full grid grid-flow-col grid-cols-${tabs.length}`}
     >
       {tabs.map((tab, i) => (
         <StyledLink key={`${tab.href}-${i}`} href={tab.href}>
