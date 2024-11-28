4f359db1fb9c296687960d6e2e96dbaae09968a2
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-28 15:26:12 -0300

    db development environment variable conditional config

--- db/db.ts
+++ db/db.ts
@@ -4,5 +4,5 @@ import postgres from 'postgres'
 // Optionally, if not using email/pass login, you can
 // use the Drizzle adapter for Auth.js / NextAuth
 // https://authjs.dev/reference/adapter/drizzle
-const client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`)
+const client = postgres(`${process.env.ENVIRONMENT === 'development' ? process.env.POSTGRES_DEVELOPMENT_URL : process.env.POSTGRES_URL!}?sslmode=require`)
 export const db = drizzle(client)
