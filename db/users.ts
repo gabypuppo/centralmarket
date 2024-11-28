328cdab8d571ef9e9a910df50101f1732a8d9b07
Ariel Brassesco <ariel.brassesco@gmail.com>
2024-11-28 16:23:40 -0300

    Punchout order message

--- db/users.ts
+++ db/users.ts
@@ -4,7 +4,10 @@ import { db } from './db'
 import { and, eq, getTableColumns, like, or } from 'drizzle-orm'
 import { getEmailDomain } from '@/utils'
 import { genSaltSync, hashSync } from 'bcrypt-ts'
-import { getOrganizationByDomain, getOrganizationWithUserMetrics } from './organizations'
+import {
+  getOrganizationByDomain,
+  getOrganizationWithUserMetrics,
+} from './organizations'
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -17,11 +20,17 @@ export const users = pgTable('users', {
   isWhiteListedUser: varchar('is_whitelisted_user', { length: 64 }),
   createdAt: varchar('created_at', { length: 64 }),
   updatedAt: varchar('updated_at', { length: 64 }),
-  isVerified: boolean('is_verified')
+  isVerified: boolean('is_verified'),
 })
 
 export type UserWithPassword = typeof users.$inferSelect
 export type User = Omit<typeof users.$inferSelect, 'password'>
+export type PunchoutData = {
+  buyerCookie: string
+  payloadID: string
+  checkoutReditrectTo: string
+}
+export type UserPunchout = User & { punchout?: PunchoutData }
 
 export async function getUser(email: string) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -53,7 +62,10 @@ export async function getUserWithPassword(email: string) {
   return res.length > 0 ? res[0] : null
 }
 
-export async function getUsersByOrganizationId(organizationId: number, where?: string) {
+export async function getUsersByOrganizationId(
+  organizationId: number,
+  where?: string,
+) {
   return await db
     .select({
       id: users.id,
@@ -61,38 +73,41 @@ export async function getUsersByOrganizationId(organizationId: number, where?: s
       lastName: users.lastName,
       email: users.email,
       role: users.role,
-      isVerified: users.isVerified
+      isVerified: users.isVerified,
     })
     .from(users)
     .where(
       where
         ? and(
-          eq(users.organizationId, organizationId),
-          or(
-            like(users.firstName, `%${where}%`),
-            like(users.lastName, `%${where}%`),
-            like(users.email, `%${where}%`)
+            eq(users.organizationId, organizationId),
+            or(
+              like(users.firstName, `%${where}%`),
+              like(users.lastName, `%${where}%`),
+              like(users.email, `%${where}%`),
+            ),
           )
-        )
-        : eq(users.organizationId, organizationId)
+        : eq(users.organizationId, organizationId),
     )
 }
 
-export async function getUserNameAndIdByOrganizationId(organizationId: number, where?: string) {
+export async function getUserNameAndIdByOrganizationId(
+  organizationId: number,
+  where?: string,
+) {
   return await db
     .select()
     .from(users)
     .where(
       where
         ? and(
-          eq(users.organizationId, organizationId),
-          or(
-            like(users.firstName, `%${where}%`),
-            like(users.lastName, `%${where}%`),
-            like(users.email, `%${where}%`)
+            eq(users.organizationId, organizationId),
+            or(
+              like(users.firstName, `%${where}%`),
+              like(users.lastName, `%${where}%`),
+              like(users.email, `%${where}%`),
+            ),
           )
-        )
-        : eq(users.organizationId, organizationId)
+        : eq(users.organizationId, organizationId),
     )
 }
 
@@ -105,21 +120,27 @@ export async function createUser(
   email: string,
   password: string,
   firstName: string,
-  lastName: string
+  lastName: string,
 ) {
   const hash = await passwordHash(password)
 
   const domain = getEmailDomain(email)
   const organization = await getOrganizationByDomain(domain)
-  const orgWithMetrics = await getOrganizationWithUserMetrics(organization?.id ?? -1)
-  
-  const role = orgWithMetrics && orgWithMetrics.metrics.users === 0 ? 'admin' : 'user'
+  const orgWithMetrics = await getOrganizationWithUserMetrics(
+    organization?.id ?? -1,
+  )
 
-  return await db
-    .insert(users)
-    .values({
-      email, password: hash, firstName, lastName, organizationId: orgWithMetrics?.id, role
-    })
+  const role =
+    orgWithMetrics && orgWithMetrics.metrics.users === 0 ? 'admin' : 'user'
+
+  return await db.insert(users).values({
+    email,
+    password: hash,
+    firstName,
+    lastName,
+    organizationId: orgWithMetrics?.id,
+    role,
+  })
 }
 
 export async function verifyUser(userId: number) {
@@ -127,11 +148,17 @@ export async function verifyUser(userId: number) {
   await db.update(users).set({ isVerified: true }).where(eq(users.id, userId))
 }
 
-export async function setUserOrganization(userId: number, organizationId: number | null) {
-  await db.update(users).set({
-    organizationId,
-    role: 'user'
-  }).where(eq(users.id, userId))
+export async function setUserOrganization(
+  userId: number,
+  organizationId: number | null,
+) {
+  await db
+    .update(users)
+    .set({
+      organizationId,
+      role: 'user',
+    })
+    .where(eq(users.id, userId))
 }
 
 export async function changePassword(userId: number, password: string) {
