a4759016f0f59492add3b371bf162b4b2291032a
Ariel Brassesco <ariel.brassesco@gmail.com>
2025-03-13 00:53:25 -0300

    run linter

--- app/api/punchout/order/route.ts
+++ app/api/punchout/order/route.ts
@@ -1,27 +1,25 @@
-import { getOrderById, getProductsByOrderId } from "@/db/orders";
-import { punchoutOrderMessage } from "@/utils/actions";
+import { getOrderById, getProductsByOrderId } from '@/db/orders'
+import { punchoutOrderMessage } from '@/utils/actions'
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const data = await request.json()
 
-  console.log("Punchout order new", { data });
+  console.log('Punchout order new', { data })
 
   const [order, products] = await Promise.all([
     getOrderById(data.orderId),
     getProductsByOrderId(data.orderId),
-  ]);
+  ])
 
-  console.log("Punchout order new", { order, products });
+  console.log('Punchout order new', { order, products })
 
   const response = await punchoutOrderMessage(
     data.punchoutData,
     order,
     products,
-  );
+  )
 
-  console.log("Punchout order new", { response });
+  console.log('Punchout order new', { response })
 
-  return new Response(JSON.stringify(response), {
-    headers: { "content-type": "application/xml" },
-  });
+  return new Response(JSON.stringify(response), { headers: { 'content-type': 'application/xml' }, })
 }
